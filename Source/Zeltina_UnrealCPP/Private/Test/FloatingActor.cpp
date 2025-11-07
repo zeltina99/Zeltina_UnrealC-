3abde82092d58553ebd4148826d6ff6e063049e8
zeltina99 <minec369@outlook.com>
2025-11-07 22:19:03 +0900

    테스트 2

--- Source/Zeltina_UnrealCPP/Private/Test/FloatingActor.cpp
+++ Source/Zeltina_UnrealCPP/Private/Test/FloatingActor.cpp
@@ -6,8 +6,14 @@
 // Sets default values
 AFloatingActor::AFloatingActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	USceneComponent* root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));	//	씬 컴포넌트 하나 만들기
+	SetRootComponent(root);	// 루트로 지정
+
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMesh"));	// 스태틱 메시 컴포넌트 하나 만들기
+	BodyMesh->SetupAttachment(root);	// root 아래에 붙이기
 
 }
 
@@ -15,13 +21,39 @@ AFloatingActor::AFloatingActor()
 void AFloatingActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	//BodyMesh->SetRelativeLocation(FVector(0, 0, 100));
+	//BodyMesh->AddRelativeLocation
+}
+
+void AFloatingActor::OnFloatingMeshUpdate(float DeltaTime)
+{
+
+	/*BodyMesh->AddRelativeLocation(DeltaTime * Speed * FVector::UpVector);
+
+	if (BodyMesh->GetRelativeLocation().Z > MoveHeight
+		|| BodyMesh->GetRelativeLocation().Z < 0)
+	{
+		Speed *= -1.0f;
+	}*/
+
+	ElapsedTime += DeltaTime;	// 시간 누적시키기
+	//UE_LOG(LogTemp, Log, TEXT("Elapsed Time : %.2f"), ElapsedTime);
+
+	// Cos 함수 이용해서 위치기준 업데이트
+	float cosValue = FMath::Cos(ElapsedTime);	// 1 -> -1 -> 1
+	cosValue += 1;				// 2 -> 0 -> 2
+	cosValue *= 0.5f;			// 1 -> 0 -> 1
+	cosValue = 1 - cosValue;	// 0 -> 1 -> 0
+
+	BodyMesh->SetRelativeLocation(FVector(0, 0, cosValue * MoveHeight));	// 위치 적용
+	BodyMesh->AddRelativeRotation(FRotator(0, SpinSpeed * DeltaTime, 0));	// 회전 적용
 }
 
 // Called every frame
 void AFloatingActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	//OnFloatingMeshUpdate(DeltaTime);
 
 }
-
--- Source/Zeltina_UnrealCPP/Public/Test/FloatingActor.h
+++ Source/Zeltina_UnrealCPP/Public/Test/FloatingActor.h
@@ -10,8 +10,8 @@ UCLASS()
 class ZELTINA_UNREALCPP_API AFloatingActor : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AFloatingActor();
 
@@ -19,8 +19,31 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+	// 스태틱 매시 움직임 업데이트하는 함수
+	virtual void OnFloatingMeshUpdate(float DeltaTime);
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+public:
+	// 위아래 이동 속도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float Speed = 100.0f;
+
+	// 최대로 올라갈 높이
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float MoveHeight = 300.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float SpinSpeed = 200.0f;
+
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> BodyMesh = nullptr;
+	//UStaticMeshComponent* BodyMesh = nullptr;
+
+	// 누적 시간
+	float ElapsedTime = 0.0f;
 
 };
