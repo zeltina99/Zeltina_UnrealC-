bc950e7263338c9541165c524f0607637d245365
zeltina99 <minec369@outlook.com>
2025-11-10 09:48:01 +0900

    입력

---
+++ Source/Zeltina_UnrealCPP/Public/Player/ActionPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ActionPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZELTINA_UNREALCPP_API AActionPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
