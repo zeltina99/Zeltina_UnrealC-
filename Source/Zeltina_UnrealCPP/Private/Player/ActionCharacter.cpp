5e8772281511bbfc7fe293f326bffae3d48986ba
zeltina99 <minec369@outlook.com>
2025-11-11 09:50:11 +0900

    주석

--- Source/Zeltina_UnrealCPP/Private/Player/ActionCharacter.cpp
+++ Source/Zeltina_UnrealCPP/Private/Player/ActionCharacter.cpp
@@ -43,7 +43,7 @@ AActionCharacter::AActionCharacter()
 	PlayerCamera->SetupAttachment(SpringArm);
 	PlayerCamera->SetRelativeRotation(FRotator(-20.0f, 0.0f, 0.0f));
 
-	bUseControllerRotationYaw = false;	// 컨트롤러의 Yaw회전을 사용안함
+	bUseControllerRotationYaw = true;	// 컨트롤러의 Yaw회전을 사용함 -> 컨트롤러의 Yaw회전을 캐릭터에 적용
 
 	//GetCharacterMovement()->bOrientRotationToMovement = true;	// 이동 방향을 바라보게 회전
 	GetCharacterMovement()->RotationRate = FRotator(0, 360, 0);
@@ -96,8 +96,8 @@ void AActionCharacter::OnMoveInput(const FInputActionValue& InValue)
 
 	FVector moveDirection(inputDirection.Y, inputDirection.X, 0.0f);
 
-	FQuat controlYawRotation = FQuat(FRotator(0, GetControlRotation().Yaw, 0));
-	moveDirection = controlYawRotation.RotateVector(moveDirection);
+	FQuat controlYawRotation = FQuat(FRotator(0, GetControlRotation().Yaw, 0));	//	컨트롤러의 Yaw회전을 따로 뽑아와서
+	moveDirection = controlYawRotation.RotateVector(moveDirection);	//	이동 방향에 적용
 
 	AddMovementInput(moveDirection);
 
