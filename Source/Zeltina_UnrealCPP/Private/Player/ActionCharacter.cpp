efeae7177b85f382e8639d3796f47ea0517a0085
zeltina99 <minec369@outlook.com>
2025-11-12 10:54:25 +0900

    내 코드로 수정

--- Source/Zeltina_UnrealCPP/Private/Player/ActionCharacter.cpp
+++ Source/Zeltina_UnrealCPP/Private/Player/ActionCharacter.cpp
@@ -3,15 +3,37 @@
 
 #include "Player/ActionCharacter.h"
 #include "EnhancedInputComponent.h"
-#include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Animation/AnimInstance.h"
 #include "Camera/CameraComponent.h"
+
+DEFINE_LOG_CATEGORY(LogGreen);
 
 // Sets default values
 AActionCharacter::AActionCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	//USpringArmComponent* spring = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring"));
+	//spring->SetupAttachment(RootComponent);
+
+	//spring->TargetArmLength = 450.0f;
+	//spring->bUsePawnControlRotation = true;
+
+	//FRotator NewRotation(0, -30, 0);
+	//spring->SetRelativeRotation(NewRotation);
+
+	//UCameraComponent* CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	//CameraComp->SetupAttachment(spring);
+	//CameraComp->bUsePawnControlRotation = false;
+
+	//bUseControllerRotationYaw = false;
+	//GetCharacterMovement()->bOrientRotationToMovement = true;
+	////GetCharacterMovement()->RotationRate = FRotator(0.f, 540.f, 0.f);
+
+	//GetCharacterMovement()->MaxWalkSpeed = 500.0f;
 
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	SpringArm->SetupAttachment(RootComponent);
@@ -23,8 +45,9 @@ AActionCharacter::AActionCharacter()
 	PlayerCamera->SetupAttachment(SpringArm);
 	PlayerCamera->SetRelativeRotation(FRotator(-20.0f, 0.0f, 0.0f));
 
-	bUseControllerRotationYaw = false;	// 컨트롤러의 Yaw 회전 사용 안함
-	GetCharacterMovement()->bOrientRotationToMovement = true;	// 이동 방향으로 캐릭터 회전
+	bUseControllerRotationYaw = false;	// 컨트롤러의 Yaw회전을 사용 안함
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;	// 이동 방향을 바라보게 회전
 	GetCharacterMovement()->RotationRate = FRotator(0, 360, 0);
 }
 
@@ -32,54 +55,17 @@ AActionCharacter::AActionCharacter()
 void AActionCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
 
 	AnimInstance = GetMesh()->GetAnimInstance();	// ABP 객체 가져오기
-
-	// 게임 진행 중에 자주 변경되는 값은 시작 시점에서 리셋을 해주는 것이 좋다.
-	CurrentStamina = MaxStamina;	// 시작할 때 최대치로 리셋
-	bIsSprint = false;
 }
 
 // Called every frame
 void AActionCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	SetStamina(DeltaTime);
 
-	// 내가 시간 누적을 직접 하는 경우
-	//TimeSinceLastStaminaUse += DeltaTime;
-	//if (TimeSinceLastStaminaUse > StaminaRegenCoolTime && CurrentStamina <= MaxStamina)
-	//{
-	//	CurrentStamina = FMath::Min(CurrentStamina + StaminaRegenAmount * DeltaTime, MaxStamina);
-	//	UE_LOG(LogTemp, Warning, TEXT("Stamina Regen : %.1f"), CurrentStamina);
-	//}
-
-	// 타이머로 조건만 설정하는 경우
-	//if (bRegenStamina)
-	//{
-	//	CurrentStamina += StaminaRegenAmount * DeltaTime;
-	//	if (CurrentStamina > MaxStamina)
-	//	{
-	//		bRegenStamina = false;
-	//		CurrentStamina = MaxStamina;
-	//	}
-	//	UE_LOG(LogTemp, Warning, TEXT("Stamina Regen : %.1f"), CurrentStamina);
-	//}
-
-	if (bIsSprint && !GetVelocity().IsNearlyZero())	// 달리기 모드인 상태에서 움직이면 스태미너를 소비한다.
-	{
-		CurrentStamina -= SprintStaminaCost * DeltaTime;
-
-		//TimeSinceLastStaminaUse = 0;
-		StaminaRegenTimerSet();
-
-		if (CurrentStamina <= 0)
-		{
-			CurrentStamina = 0.0f;
-			SetWalkMode();
-		}
-
-		UE_LOG(LogTemp, Warning, TEXT("Stamina : %.1f"), CurrentStamina);
-	}
 }
 
 // Called to bind functionality to input
@@ -88,7 +74,7 @@ void AActionCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	UEnhancedInputComponent* enhanced = Cast<UEnhancedInputComponent>(PlayerInputComponent);
-	if (enhanced)	// 입력 컴포넌트가 향상된 입력 컴포넌트일 때
+	if (enhanced)	//	입력 컴포넌트가 향상된 입력 컴포넌트일 때
 	{
 		enhanced->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AActionCharacter::OnMoveInput);
 		enhanced->BindActionValueLambda(IA_Sprint, ETriggerEvent::Started,
@@ -99,6 +85,10 @@ void AActionCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 			[this](const FInputActionValue& _) {
 				SetWalkMode();
 			});
+		/*enhanced->BindAction(IA_Sprint, ETriggerEvent::Started, this, &AActionCharacter::OnSprintStarted);
+		enhanced->BindAction(IA_Sprint, ETriggerEvent::Completed, this, &AActionCharacter::OnSprintEnded);
+		enhanced->BindAction(IA_Sprint, ETriggerEvent::Canceled, this, &AActionCharacter::OnSprintEnded);*/
+		enhanced->BindAction(IA_Attack, ETriggerEvent::Started, this, &AActionCharacter::OnAttackStarted);
 		enhanced->BindAction(IA_Roll, ETriggerEvent::Triggered, this, &AActionCharacter::OnRollInput);
 	}
 }
@@ -108,85 +98,107 @@ void AActionCharacter::OnMoveInput(const FInputActionValue& InValue)
 	FVector2D inputDirection = InValue.Get<FVector2D>();
 	//UE_LOG(LogTemp, Log, TEXT("Dir : (%.1f, %.1f)"), inputDirection.X, inputDirection.Y);
 	//UE_LOG(LogTemp, Log, TEXT("Dir : (%s)"), *inputDirection.ToString());
+
+	/*FRotator ControlRot = Controller->GetControlRotation();
+	FRotator YawRot(0.f, ControlRot.Yaw, 0.f);
+
+	FVector ForwardDir = FRotationMatrix(YawRot).GetUnitAxis(EAxis::X);
+	FVector RightDir = FRotationMatrix(YawRot).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(ForwardDir, inputDirection.Y);
+	AddMovementInput(RightDir, inputDirection.X);*/
+
 	FVector moveDirection(inputDirection.Y, inputDirection.X, 0.0f);
 
-	FQuat controlYawRotation = FQuat(FRotator(0, GetControlRotation().Yaw, 0));	// 컨트롤러의 Yaw회전을 따로 뽑아와서
-	moveDirection = controlYawRotation.RotateVector(moveDirection);	// 이동 방향에 적용
+	FQuat controlYawRotation = FQuat(FRotator(0, GetControlRotation().Yaw, 0));	//	컨트롤러의 Yaw회전을 따로 뽑아와서
+	moveDirection = controlYawRotation.RotateVector(moveDirection);	//	이동 방향에 적용
 
 	AddMovementInput(moveDirection);
 
 }
 
+//void AActionCharacter::OnSprintStarted(const FInputActionValue& InValue)
+//{
+//	GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+//}
+//
+//void AActionCharacter::OnSprintEnded(const FInputActionValue& InValue)
+//{
+//	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+//}
+
+void AActionCharacter::OnAttackStarted(const FInputActionValue& InValue)
+{
+	if (USkeletalMeshComponent* SkelMesh = GetMesh())
+	{
+		if (UAnimInstance* Anim = SkelMesh->GetAnimInstance())
+		{
+			if (!Anim->Montage_IsPlaying(AttackMontage))
+			{
+				Anim->Montage_Play(AttackMontage, 1.0f);
+			}
+		}
+	}
+}
+
 void AActionCharacter::OnRollInput(const FInputActionValue& InValue)
 {
-	if (AnimInstance.IsValid())
+	if (Stamina > 5.0f)
 	{
-		if (!AnimInstance->IsAnyMontagePlaying()
-			&& CurrentStamina >= RollStaminaCost)
+		if (AnimInstance.IsValid())
 		{
-			//if (!GetLastMovementInputVector().IsNearlyZero())	// 입력을 하는 중에만 즉시 회전
-			//{
-			//	SetActorRotation(GetLastMovementInputVector().Rotation());	// 마지막 입력 방향으로 즉시 회전 시키기
-			//}
-			CurrentStamina -= RollStaminaCost;
-			//TimeSinceLastStaminaUse = 0;
-			StaminaRegenTimerSet();
-
-			UE_LOG(LogTemp, Warning, TEXT("Stamina : %.1f"), CurrentStamina);
-			PlayAnimMontage(RollMontage);
+			if (!AnimInstance->IsAnyMontagePlaying())
+			{
+				if (!GetLastMovementInputVector().IsNearlyZero())	//	입력을 하는 중에만 즉시 회전
+				{
+					SetActorRotation(GetLastMovementInputVector().Rotation());	//	마지막 입력 방향으로 즉시 회전 시키기
+				}
+				PlayAnimMontage(RollMontage);
+				Stamina -= 5.0f;
+				//UE_LOG(LogTemp, Warning, TEXT("Stamina : %.1f"), Stamina);
+			}
 		}
 	}
 }
 
 void AActionCharacter::SetSprintMode()
 {
-	//UE_LOG(LogTemp, Warning, TEXT("달리기 모드"));
-	GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
-	bIsSprint = true;
+	OnStamina = true;
+	if (Stamina > 0)
+	{
+		GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+	}
+
 }
 
 void AActionCharacter::SetWalkMode()
 {
+	OnStamina = false;
 	//UE_LOG(LogTemp, Warning, TEXT("걷기 모드"));
 	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
-	bIsSprint = false;
 }
 
-void AActionCharacter::StaminaRegenTimerSet()
+void AActionCharacter::SetStamina(float DeltaTime)
 {
-	//UWorld* world = GetWorld();
-	//FTimerManager& timerManager = world->GetTimerManager();
-
-	//GetWorldTimerManager().ClearTimer(StaminaCoolTimer);	// 해서 나쁠 것은 없음(SetTimer할 때 이미 내부적으로 처리하고 있다)
-	GetWorldTimerManager().SetTimer(
-		StaminaCoolTimer,
-		[this]() {
-			//bRegenStamina = true;
-			UE_LOG(LogTemp, Log, TEXT("리젠 타이머 실행"));
-
-			GetWorldTimerManager().SetTimer(
-				StaminaRegenTimer,
-				this,
-				&AActionCharacter::StaminaRegenPerTick,
-				0.1f,	// 실행 간격
-				true,	// 반복 재생 여부
-				0.1f);	// 첫 딜레이
-		},
-		StaminaRegenCoolTime,
-		false);
-}
-
-void AActionCharacter::StaminaRegenPerTick()
-{
-	CurrentStamina += StaminaRegenAmountPerTick;	// 틱당 10
-	//CurrentStamina += MaxStamina * StaminaRegenRatePerTick;	// 틱당 최대 스태미너의 10%
-
-	if (CurrentStamina > MaxStamina)
+	//UE_LOG(LogGreen, Display, TEXT("Stamina : %.1f"), Stamina);
+	if (OnStamina == true)
 	{
-		CurrentStamina = MaxStamina;
-		GetWorldTimerManager().ClearTimer(StaminaRegenTimer);
+		if (Stamina > 0)
+		{
+			Stamina -= DeltaTime;
+		}
+		else
+		{
+			SetWalkMode();
+		}
+		Delay = 0.0f;
 	}
-
-	UE_LOG(LogTemp, Warning, TEXT("Stamina Regen : %.1f"), CurrentStamina);
+	else
+	{
+		Delay += DeltaTime;
+		if (Delay >= 3.0f && Stamina < 10.0f)
+		{
+			Stamina += DeltaTime;
+		}
+	}
 }
-
--- Source/Zeltina_UnrealCPP/Public/Player/ActionCharacter.h
+++ Source/Zeltina_UnrealCPP/Public/Player/ActionCharacter.h
@@ -5,10 +5,15 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "InputActionValue.h"
+#include "Animation/AnimMontage.h"
 #include "ActionCharacter.generated.h"
 
 class UInputAction;
 //class USpringArmComponent;
+class UCameraComponent;
+class UAnimMontage;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogGreen, Log, All);
 
 UCLASS()
 class ZELTINA_UNREALCPP_API AActionCharacter : public ACharacter
@@ -31,90 +36,56 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
-	// 이동 방향 입력 받기
 	void OnMoveInput(const FInputActionValue& InValue);
-
-	// 구르기 입력 받기
+	/*void OnSprintStarted(const FInputActionValue& InValue);
+	void OnSprintEnded(const FInputActionValue& InValue);*/
+	void OnAttackStarted(const FInputActionValue& InValue);
 	void OnRollInput(const FInputActionValue& InValue);
 
-	// 달리기 모드 설정
 	void SetSprintMode();
-
-	// 걷기 모드 설정
 	void SetWalkMode();
-
-private:
-	void StaminaRegenTimerSet();
-	void StaminaRegenPerTick();
+	void SetStamina(float DeltaTime);
 
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player|Camera")
 	TObjectPtr<class USpringArmComponent> SpringArm = nullptr;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player|Camera")
 	TObjectPtr<class UCameraComponent> PlayerCamera = nullptr;
 
-	// 인풋 액션들
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	TObjectPtr<UInputAction> IA_Move = nullptr;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	TObjectPtr<UInputAction> IA_Sprint = nullptr;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+
+	TObjectPtr<UInputAction> IA_Attack = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+
 	TObjectPtr<UInputAction> IA_Roll = nullptr;
 
-	// 달리기 속도
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player|Movement")
-	float SprintSpeed = 1200.0f;
-	// 걷기 속도
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player|Movement")
-	float WalkSpeed = 600.0f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation|Montage")
+	TObjectPtr<UAnimMontage> AttackMontage = nullptr;
 
-	// 구르기 몽타주
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player|Movement")
+	float WalkSpeed = 600.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player|Movement")
+	float SprintSpeed = 1200.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player|State")
+	float Stamina = 10.0f;
+	float Delay = 0.0f;
+	bool OnStamina = false;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation|Montage")
 	TObjectPtr<UAnimMontage> RollMontage = nullptr;
-
-	// 현재 스태미너
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player|Resource")
-	float CurrentStamina = 100.0f;
-
-	// 최대 스태미너
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player|Resource")
-	float MaxStamina = 100.0f;
-
-	// 달리기 상태일 때 초당 스태미너 비용
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player|Resource")
-	float SprintStaminaCost = 20.0f;
-
-	// 구르기를 하기 위해 필요한 스태미너 비용
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player|Resource")
-	float RollStaminaCost = 50.0f;
-
-	// 스테미너가 자동 회복되는데 걸리는 시간
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player|Resource")
-	float StaminaRegenCoolTime = 3.0f;
-
-	// 스태미너 자동 회복양(초당)
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player|Resource")
-	float StaminaRegenAmount = 50.0f;
-
-	// 스태미너 자동 회복양(틱당)
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player|Resource")
-	float StaminaRegenAmountPerTick = 10.0f;
-
-	// 스태미너 자동 회복양(틱당, 최대치의 %)
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player|Resource")
-	float StaminaRegenRatePerTick = 0.1f;
-
-	// 플레이어가 뛰고 있는 중인지 표시 해놓은 변수
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Player|State")
-	bool bIsSprint = false;
 
 private:
 	UPROPERTY()
 	TWeakObjectPtr<UAnimInstance> AnimInstance = nullptr;
-
-	float TimeSinceLastStaminaUse = 0.0f;
-
-	FTimerHandle StaminaCoolTimer;
-	FTimerHandle StaminaRegenTimer;
-	bool bRegenStamina = false;
 };
