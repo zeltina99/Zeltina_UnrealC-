2476a78587c3871060dadc52237657dc4a56fb56
zeltina99 <minec369@outlook.com>
2025-11-13 10:45:50 +0900

    UAnimNotifyState_SectionJump추가

---
+++ Source/Zeltina_UnrealCPP/Public/AnimNotify/AnimNotifyState_SectionJump.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyState_SectionJump.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZELTINA_UNREALCPP_API UAnimNotifyState_SectionJump : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
