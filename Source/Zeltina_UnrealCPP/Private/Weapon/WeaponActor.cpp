a292761e5f244828ed0c883af050841d319f1468
zeltina99 <minec369@outlook.com>
2025-11-21 17:59:23 +0900

    커밋

--- Source/Zeltina_UnrealCPP/Private/Weapon/WeaponActor.cpp
+++ Source/Zeltina_UnrealCPP/Private/Weapon/WeaponActor.cpp
@@ -64,9 +64,9 @@ void AWeaponActor::DamageToTarget(AActor* InTarget)
 		instigator = WeaponOwner->GetController();
 	}
 
-	UE_LOG(LogTemp, Warning, TEXT("[Weapon] DamageToTarget Called -> Target=%s, Damage=%.1f"),
+	/*UE_LOG(LogTemp, Warning, TEXT("[Weapon] DamageToTarget Called -> Target=%s, Damage=%.1f"),
 		InTarget ? *InTarget->GetName() : TEXT("NULL"),
-		finalDamage);
+		finalDamage);*/
 	//UE_LOG(LogTemp, Log, TEXT("Overlapped : %s"), *OtherActor->GetName());
 	UGameplayStatics::ApplyDamage(InTarget, finalDamage, instigator, this, DamageType);
 }
