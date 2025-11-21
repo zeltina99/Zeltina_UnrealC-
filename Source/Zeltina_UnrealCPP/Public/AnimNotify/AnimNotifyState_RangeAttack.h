c97bc3fa1023674c22c822695d02816d7134317d
zeltina99 <minec369@outlook.com>
2025-11-21 10:22:31 +0900

    세번째 콤보 실행 했을 때 범위 공격 실습

---
+++ Source/Zeltina_UnrealCPP/Public/AnimNotify/AnimNotifyState_RangeAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyState_RangeAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZELTINA_UNREALCPP_API UAnimNotifyState_RangeAttack : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
