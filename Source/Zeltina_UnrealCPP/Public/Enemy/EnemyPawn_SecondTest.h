198205184085a19984ead85610c123bbe4992d08
zeltina99 <minec369@outlook.com>
2025-11-24 15:32:49 +0900

    파생 클래스와 enemycount 서브시스템 클래스 추가

---
+++ Source/Zeltina_UnrealCPP/Public/Enemy/EnemyPawn_SecondTest.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/EnemyPawn_Test.h"
+#include "EnemyPawn_SecondTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZELTINA_UNREALCPP_API AEnemyPawn_SecondTest : public AEnemyPawn_Test
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Zeltina_UnrealCPP/Public/Framework/EnemyCountSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "EnemyCountSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZELTINA_UNREALCPP_API UEnemyCountSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+};
